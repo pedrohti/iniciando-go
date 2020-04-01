02d3e91498fbaf67f4f38e9941897bd7025ef799
Pedro Henrique Moraes da Silva <pedro.henrique@foa.org.br>
2020-04-01 17:28:52 -0300

    :construction: First Commit

---
+++ src/hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Pedro"
+	versao := 1.1
+	fmt.Println("Olá, Sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+}
