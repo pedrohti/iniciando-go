f640c81b9ee7267b4021c26ec934e3a9bab2c6ce
pedrohti <pedroh.ti@hotmail.com>
2020-04-01 19:02:45 -0300

    :rocket: Criando funções

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,18 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	exibeIntroducao()
+	exibeMenu()
+	comando := lerComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando")
+	case 2:
+		fmt.Println("Logs Exibidos")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando Inválido!")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Pedro"
 	versao := 1.1
+
 	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
+func lerComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
