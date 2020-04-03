a1d487b882c55d8b8b93c50b05b945a7ba05f2d6
pedrohti <pedroh.ti@hotmail.com>
2020-04-03 19:01:00 -0300

    :sparkles: Adicionado a função de loop no menu e feito o Monitoramento

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,32 +2,49 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := lerComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando")
-	case 2:
-		fmt.Println("Logs Exibidos")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando Inválido!")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		// _, idade := devolveNome() //com underline ignora um ou mais valores
+		// fmt.Println(idade)
+
+		comando := lerComando()
+
+		switch comando {
+		case 1:
+			inciciarMonitoramento()
+		case 2:
+			fmt.Println("Logs Exibidos")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando Inválido!")
+			os.Exit(-1)
+		}
 	}
 }
 
+// func devolveNome() (string, int) { //função de exemplo para exibir o returno multiplo de uma função
+// 	nome := "Pedro"
+// 	idade := 27
+// 	return nome, idade
+// }
+
 func exibeIntroducao() {
 	nome := "Pedro"
-	versao := 1.1
+	versao := 1.2
 
 	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
@@ -43,6 +60,37 @@ func lerComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
+}
+
+func inciciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{
+		"https://www.alura.com.br",
+		"https://random-status-code.herokuapp.com/",
+		"https://www.covidvisualizer.com/",
+	}
+
+	for i := 0; i < monitoramentos; i++ {
+		for indice, item := range sites {
+			fmt.Println("Testando site", indice, ":", item)
+			testaSite(item)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("O site", site, "carregado com sucesso!")
+	} else {
+		fmt.Println("O site", site, "não está carregando! Erro:", resp.StatusCode)
+	}
 }
